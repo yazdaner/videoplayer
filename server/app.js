8e431faa2e721ddd2e3a084820fdb438df190b86
yazdan <yazdaner8@gmail.com>
2024-12-05 23:35:16 +0330

    videoplayer 1.0.0

--- server/app.js
+++ server/app.js
@@ -1,8 +1,31 @@
-const express = require('express');
+const { log } = require("console");
+const express = require("express");
+const fs = require("fs");
 const app = express();
 
-app.get('/videos',function(request,response){
-    return response.send('node application');
+app.get("/ads", function (request, response) {
+  let range = request.headers.range;
+  if (!range) {
+    range = "bytes=0-";
+  // response.status(400).send("bad request");
+  }
+  const videoPath = "../videos/a.mp4";
+  const videoSize = fs.statSync(videoPath).size;
+  const chunkSize = 10 ** 6;
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + chunkSize, videoSize - 1);
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "content-type": "video/mp4",
+  };
+
+  response.writeHead(206, headers);
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+  videoStream.pipe(response);
+  
 });
 
 app.listen(8000);
